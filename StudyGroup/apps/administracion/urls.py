6b7e8d8ee19c13c731930617c2f0c2824194b5ac
Iván de León <ivandeleon@solucionestip.com>
2015-04-23 00:56:17 -0600

    1- agregar formularui 2. Agregar vista basada en clases para ver el formulario

--- StudyGroup/apps/administracion/urls.py
+++ StudyGroup/apps/administracion/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import include, url
-from .views import IndexView
+from .views import  RegistrarEstudianteView
 
 urlpatterns = [
-	 url(r'^home/',IndexView.as_view()),
+	# url(r'^home/',IndexView.as_view()),
+	 url(r'^registrar/$',RegistrarEstudianteView.as_view(), name="Registrar_Estudiante"),
    
 ]
--- StudyGroup/apps/administracion/views.py
+++ StudyGroup/apps/administracion/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView,CreateView
+from .models import Estudiante
 # Create your views here.
 
 
-class IndexView(TemplateView):
-	template_name='administracion/index.html'
+#class IndexView(TemplateView):
+#	template_name='administracion/index.html'
+
+class RegistrarEstudianteView(CreateView):
+	template_name = 'administracion/index.html'
+	model = Estudiante
+	success_url = 
