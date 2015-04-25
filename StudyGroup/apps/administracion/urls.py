3f46e8deae8af0bc0945915e6c6f4c21a3fe4fd7
Iván de León <ivandeleon@solucionestip.com>
2015-04-24 21:26:44 -0600

    1. Agregar la url para editar y eliminar estudiantes 2. Agregar la vista para editar y eliminar estudiantes 3. Agregar el template en blanco para elimiar estudiantes

--- StudyGroup/apps/administracion/urls.py
+++ StudyGroup/apps/administracion/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import include, url
-from .views import  RegistrarEstudianteView,HomeView,ListarEstudianteView
-
+from .views import  RegistrarEstudianteView,HomeView,ListarEstudianteView,VerEstudianteView, DeleteEstudianteView
 urlpatterns = [
 	# url(r'^home/',IndexView.as_view()),
 	 url(r'^home/$',HomeView.as_view(), name="Home"),
    	 url(r'^registrar/$',RegistrarEstudianteView.as_view(), name="Registrar_Estudiante"),
    	 url(r'^listaestudiantes/$',ListarEstudianteView.as_view(), name="Listar_Estudiante"),
+   	 url(r'^estudiante/(?P<pk>\d+)/$',VerEstudianteView.as_view(), name="Ver_Estudiante"),
+   	 url(r'^estudiantedel/(?P<pk>\d+)/$',DeleteEstudianteView.as_view(), name="Eliminar_Estudiante"),
 ]
--- StudyGroup/apps/administracion/views.py
+++ StudyGroup/apps/administracion/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView,CreateView,ListView
+from django.views.generic import TemplateView,CreateView,ListView,DetailView,DeleteView
 from django.core.urlresolvers import reverse_lazy
 from .models import Estudiante
 
@@ -19,7 +19,18 @@ class RegistrarEstudianteView(CreateView):
 class HomeView(TemplateView):
 	template_name = "administracion/index.html"
 
+
 class ListarEstudianteView(ListView):
 	template_name = "administracion/listarestudiante.html"
 	paginate_by = 5
 	model = Estudiante
+
+class VerEstudianteView(DetailView):
+	model = Estudiante
+	template_name = "administracion/verestudiante.html"
+	context_object_name="Estudiante"
+
+
+class DeleteEstudianteView(DeleteView):
+	model = Estudiante
+	success_url = reverse_lazy('adm:Listar_Estudiante')
